5bf86474291e8bbafc087bac33fca4304c74de17
SatoruGojoMaisForte <satorumec@gmail.com>
2024-10-14 15:47:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-import requests
-from kivy.animation import Animation
-from kivy.core.window import Window
 from kivy.lang.builder import Builder
-from kivy.properties import StringProperty, ListProperty
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
-from kivymd.theming import ThemeManager
-from libs.screens.home_page.homepage import HomePage
 from libs.screens.Cadastro.cadastro import Cadastro
-from libs.screens.perfil_page.perfilpage import PerfilPage
-from libs.screens.outros_perfis_page.outrospage import OutrosPage
+
 
 class PrincipalApp(MDApp):
 
     def build(self):
         self.load_all_kv_files()
         self.screenmanager = ScreenManager()
-        self.screenmanager.add_widget(Cadastro(name=''))
+        self.screenmanager.add_widget(Cadastro(name='cadastro'))
 
         return self.screenmanager
 
