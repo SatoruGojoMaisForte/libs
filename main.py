139079915a654be27b894e7f880792ba1aa03ad8
SatoruGojoMaisForte <satorumec@gmail.com>
2024-10-21 17:51:18 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
 from kivy.core.window import Window
-from libs.screens.Cadastro.cadastro import Cadastro
+from libs.screens.login_screen.loginscreen import LoginScreen
 
 
 class PrincipalApp(MDApp):
 
     def build(self):
         self.load_all_kv_files()
-        Window.size = (400, 700)
         self.screenmanager = ScreenManager()
-        self.screenmanager.add_widget(Cadastro(name='cadastro'))
+        self.screenmanager.add_widget(LoginScreen(name='Login'))
 
         return self.screenmanager
 
     def load_all_kv_files(self):
-        Builder.load_file('libs/screens/Cadastro/cadastro.kv')
+        Builder.load_file('libs/screens/login_screen/loginscreen.kv')
 
 PrincipalApp().run()
