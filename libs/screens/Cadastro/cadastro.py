858af0b4bb067ac660c77f8a9d1ae933d0e163f9
SatoruGojoMaisForte <satorumec@gmail.com>
2024-10-23 16:53:38 -0300

    Update cadastro.py

--- libs/screens/Cadastro/cadastro.py
+++ libs/screens/Cadastro/cadastro.py
@@ -1,139 +1,281 @@
-from kivy.network.urlrequest import UrlRequest
-from queue import Queue
-import re
+<Cadastro>:
+    name: 'cadastro'
+    md_bg_color: 1, 1, 1, 1
 
-from kivy.utils import get_color_from_hex
-from kivymd.uix.relativelayout import MDRelativeLayout
-from kivymd.uix.screen import MDScreen
-from kivymd.uix.snackbar import MDSnackbarText, MDSnackbar, MDSnackbarCloseButton
-from kivymd.material_resources import dp
+    MDFloatLayout:
+        id: centro
 
-class Cadastro(MDScreen):
-    cadastrar = False
-    email = False
+        MDLabel:
+            pos_hint: {'center_y': 0.95, 'right': 1.05}
+            theme_text_color: 'Custom'
+            text_color: 'black'
+            text: 'Chatto'
+            halign: 'left'
+            valign: 'center'
+            theme_font_size: 'Custom'
+            font_size: '40sp'
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.result_queue = Queue()
 
-    def login(self):
-        self.manager.current = 'Login'
-    def sneck(self):
-        snack = MDSnackbar(
-            MDSnackbarText(
-                text="Usuário cadastrado",
-                theme_text_color='Custom',
-                bold=True,
-                text_color='white'
-            ),
-            MDRelativeLayout(
-                MDSnackbarCloseButton(
-                    icon='check',
-                    theme_icon_color='Custom',
-                    icon_color='white',
-                    pos_hint={"center_x": 0.5}
+        MDLabel:
+            text: "Junte-se a nós! Cadastre-se agora e desbloqueie uma experiência única e personalizada."
+            size_hint: 0.9, 0.5
+            font_style: "Title"
+            theme_text_color: 'Custom'
+            text_color: 'black'
+            role: "small"
+            pos_hint: {'center_y': 0.83, 'center_x': 0.5}
+            spacing: 10
 
-                ),
-            ),
-            y=dp(150),
-            orientation='vertical',
-            pos_hint={"center_x": 0.5, 'center_y': 0.1},
-            size_hint_x=0.8,
-        )
-        snack.background_color = get_color_from_hex('#00ff08')
-        snack.open()
+        MDCard:
+            pos_hint: {"center_y": 0.66, "center_x": 0.5}
+            size_hint: 0.87, 0.1
+            theme_bg_color: 'Custom'
+            md_bg_color: 1, 1, 1, 1
 
-    def verificar_formato(self, email: str) -> bool:
-        regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-        return bool(re.match(regex, email))
+            MDRelativeLayout:
+                MDTextField:
+                    id: nome
+                    theme_line_color: 'Custom'
+                    line_color_normal: 'black'
+                    line_color_focus: 'blue'
+                    theme_text_color: 'Custom'
+                    text_color_focus: 'black'
+                    text_color_normal: 'grey'
+                    pos_hint: {"center_y": 0.65, "center_x": 0.5}
+                    size_hint: 1, None
+                    icon_right: "account"
+                    mode: "outlined"
+                    #radius: 30, 30
 
-    def verificar_nome(self):
-        nome = self.ids.nome.text
-        url = f"https://api-name.onrender.com/check-name?name={nome}"
-        UrlRequest(
-            url,
-            on_success=self.verificar_nome_sucesso,
-            on_error=self.api_erro,
-            on_failure=self.api_erro,
-            method='GET'
-        )
+                    MDTextFieldLeadingIcon:
+                        icon: "account-circle-outline"
+                        theme_icon_color: 'Custom'
+                        icon_color_normal: 'black'
+                        icon_color_focus: 'blue'
 
-    def verificar_nome_sucesso(self, req, result):
-        data = result
-        print(f'response nome: {data}')
-        if not data['exists']:
-            self.ids.erro_nome.text = ''
-            self.verificar_email(self.ids.email.text)
-            self.email = True
+                    MDTextFieldHintText:
+                        theme_text_color: 'Custom'
+                        text_color_normal: 'black'
+                        text_color_focus: 'grey'
+                        text: 'Nome do usuario'
 
-        else:
-            self.ids.erro_nome.text = 'Usuário já cadastrado'
-            self.ids.erro_nome.text_color = 'red'
-            self.verificar_email(self.ids.email.text)
-            self.email = False
-            print(True)
+                MDLabel:
+                    id: erro_nome
+                    text: ''
+                    font_style: 'Title'
+                    role: 'small'
+                    theme_text_color: 'Custom'
+                    text_color: 'red'
+                    halign: 'right'
+                    pos_hint: {'center_y': 0.1}
 
-    def verificar_email(self, email):
-        if not self.verificar_formato(email):
-            self.ids.erro_email.text = 'Formato de email inválido'
-            self.ids.erro_email.text_color = 'red'
-            return
-        url = f"https://api-email-2gyg.onrender.com/check-email?email={email}"
-        UrlRequest(
-            url,
-            on_success=self.verificar_email_sucesso,
-            on_error=self.api_erro,
-            on_failure=self.api_erro,
-            method='GET'
-        )
+        MDCard:
+            pos_hint: {"center_y": 0.54, "center_x": 0.5}
+            size_hint: 0.87, 0.1
+            theme_bg_color: 'Custom'
+            md_bg_color: 1, 1, 1, 1
 
-    def verificar_email_sucesso(self, req, result):
-        data = result
-        print(f"response email: {result}")
-        if data['existir']:
-            self.ids.erro_email.text = 'Email já cadastrado'
-            self.ids.erro_email.text_color = 'red'
-        else:
-            self.ids.erro_email.text = ''
-            print(f"pode adicionar usuario: {self.email}")
-            if self.email:
-                self.adicionar_usuario()
-            else:
-                pass
+            MDRelativeLayout:
+                MDTextField:
+                    id: email
+                    theme_line_color: 'Custom'
+                    line_color_normal: 'black'
+                    theme_text_color: 'Custom'
+                    text_color_focus: 'black'
+                    text_color_normal: 'grey'
+                    line_color_focus: 'blue'
+                    theme_bg_color: "Custom"
+                    md_bg_color: 0, 1, 1, 1
+                    pos_hint: {"center_y": 0.65, "center_x": 0.5}
+                    size_hint: 1, None
+                    icon_right: "account"
+                    mode: "outlined"
+                    #radius: 30, 30
 
-    def adicionar_usuario(self):
-        url = f'https://api-add-user.onrender.com/add-user?nome={self.ids.nome.text}&email={self.ids.email.text}&senha={self.ids.senha.text}'
-        UrlRequest(url, req_body=None, on_success=self.adicionar_usuario_sucesso, on_error=self.api_erro, on_failure=self.api_erro, method='POST')
+                    MDTextFieldLeadingIcon:
+                        icon: "email"
+                        theme_icon_color: 'Custom'
+                        icon_color_normal: 'black'
+                        icon_color_focus: 'blue'
 
-    def adicionar_usuario_sucesso(self, req, result):
-        data = result
-        print(f'response adicionar_usuario: {data}')
-        if data['message'] == 'O usuario foi adicionado com sucesso':
-            self.sneck()
-            self.ids.nome.text = ''
-            self.ids.email.text = ''
-            self.ids.senha.text = ''
-        else:
-            print('Falha ao adicionar o usuário')
+                    MDTextFieldHintText:
+                        theme_text_color: 'Custom'
+                        text_color_normal: 'black'
+                        text_color_focus: 'grey'
+                        text: 'Email'
 
-    def api_erro(self, req, error):
-        pass
+                MDLabel:
+                    id: erro_email
+                    text: ''
+                    font_style: 'Title'
+                    role: 'small'
+                    theme_text_color: 'Custom'
+                    text_color: 'red'
+                    pos_hint: {'center_y': 0.1}
+                    halign: 'right'
 
-    def logica(self):
-        nome = self.ids.nome.text
-        email = self.ids.email.text
-        senha = self.ids.senha.text
+        MDCard:
+            pos_hint: {"center_y": 0.42, "center_x": 0.5}
+            size_hint: 0.87, 0.1
+            theme_bg_color: 'Custom'
+            md_bg_color: 1, 1, 1, 1
 
-        # Verificação inicial dos campos
-        if nome == '':
-            self.ids.nome.focus = True
-            return
-        elif email == '' and nome != '':
-            self.ids.email.focus = True
-            return
-        elif senha == '' and nome != '' and email != '':
-            self.ids.senha.focus = True
-            return
+            MDRelativeLayout:
+                MDTextField:
+                    id: senha
+                    theme_line_color: 'Custom'
+                    line_color_normal: 'black'
+                    line_color_focus: 'blue'
+                    theme_text_color: 'Custom'
+                    text_color_focus: 'black'
+                    text_color_normal: 'grey'
+                    pos_hint: {"center_y": 0.65, "center_x": 0.5}
+                    size_hint: 1, None
+                    icon_right: "account"
+                    mode: "outlined"
+                    #radius: 30, 30
 
-        # Iniciar verificações de nome e email
-        self.verificar_nome()
+                    MDTextFieldLeadingIcon:
+                        icon: "lock"
+                        theme_icon_color: 'Custom'
+                        icon_color_normal: 'black'
+                        icon_color_focus: 'blue'
+
+                    MDTextFieldHintText:
+                        theme_text_color: 'Custom'
+                        text_color_normal: 'black'
+                        text_color_focus: 'grey'
+                        text: 'Senha'
+
+                MDLabel:
+                    id: erro_senha
+                    text: ''
+                    font_style: 'Title'
+                    role: 'small'
+                    theme_text_color: 'Custom'
+                    text_color: 'red'
+                    pos_hint: {'center_y': 0.1}
+                    halign: 'right'
+
+        MDCard:
+            id: glowing_card
+            style: 'elevated'
+            size_hint: 0.85, 0.07
+            pos_hint: {"center_y": 0.29, "center_x": 0.5}
+            padding: "16dp"
+            theme_shadow_color: "Custom"
+            shadow_color: "black"
+            theme_line_color: 'Custom'
+            line_color: 'black'
+            theme_bg_color: "Custom"
+            md_bg_color: 0, 0, 0, 1
+            md_bg_color_disabled: "grey"
+            theme_shadow_offset: "Custom"
+            shadow_offset: (1, -2)
+            theme_shadow_softness: "Custom"
+            shadow_softness: 1
+            theme_elevation_level: "Custom"
+            elevation_level: 2
+            on_release: root.logica()
+
+            radius: [15]
+
+            BoxLayout:
+                size_hint: 1, 1
+                orientation: 'vertical'
+                padding: 0
+                spacing: 0
+
+                MDLabel:
+
+                    text: "Cadastrar"
+                    theme_text_color: "Custom"
+                    text_color: "white"
+                    pos_hint: {"center_x": 0.5, "center_y": 0.5}
+                    halign: 'center'
+                    font_style: "Title"
+                    bold: True
+
+        MDCard:
+            id: card
+            style: 'elevated'
+            size_hint: 1, 0.06
+            padding: "16dp"
+            theme_shadow_color: "Custom"
+            pos_hint: {'center_x': 0.35, 'center_y': 0.13}
+            shadow_color: "white"
+            theme_line_color: 'Custom'
+            line_color: 'white'
+            theme_bg_color: "Custom"
+            md_bg_color: 1, 1, 1, 1
+            md_bg_color_disabled: "grey"
+            theme_shadow_offset: "Custom"
+            shadow_offset: (1, -2)
+            theme_shadow_softness: "Custom"
+            shadow_softness: 1
+            theme_elevation_level: "Custom"
+            elevation_level: 2
+            on_release: root.login()
+
+            MDRelativeLayout:
+                MDLabel:
+                    text: "Tem uma conta?"
+                    theme_text_color: "Custom"
+                    pos_hint: {"center_y": 0.5, "center_x": 0.5}
+                    text_color: "black"
+                    font_style: "Title"
+                    bold: True
+                    halign: 'center'
+                    role: "medium"
+
+                MDLabel:
+                    pos_hint: {'center_x': 0.38, 'center_y': 0.5}
+                    text: 'Faça login'
+                    font_style: "Title"
+                    role: "medium"
+                    halign: 'right'
+                    bold: True
+                    theme_text_color: "Custom"
+                    text_color: "blue"
+
+        MDCard:
+            id: carregando
+            style: 'elevated'
+            size_hint: 0.00001, 0.000001
+            pos_hint: {"center_y": 0.92, "center_x": 1.05}
+            padding: "16dp"
+            # Sets custom properties.
+            theme_bg_color: "Custom"
+            md_bg_color: 1, 1, 1, 1
+            md_bg_color_disabled: "grey"
+            theme_shadow_offset: "Custom"
+            shadow_offset: (1, -2)
+            theme_shadow_softness: "Custom"
+            shadow_softness: 1
+            theme_elevation_level: "Custom"
+            elevation_level: 2
+
+            MDRelativeLayout:
+                id: carregando2
+
+                MDCircularProgressIndicator:
+                    id: progresso
+                    size_hint: None, None
+                    size: "60dp", "60dp"
+                    max_value: 100
+                    value: 0
+                    pos_hint: {'center_x': .5, 'center_y': .6}
+
+                MDLabel:
+                    id: texto_carregando
+                    text: 'Carregando...'
+                    font_style: 'Title'
+                    role: 'medium'
+                    halign: 'center'
+                    bold: True
+                    theme_text_color: 'Custom'
+                    text_color: 'black'
+                    pos_hint: {'center_x': .5, 'center_y': .5}
+
+
+
