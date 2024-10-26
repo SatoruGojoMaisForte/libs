de0123f4fc4faae0786daac6e7f68aa9848abf67
SatoruGojoMaisForte <satorumec@gmail.com>
2024-10-26 16:51:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,14 +15,10 @@ class PrincipalApp(MDApp):
         self.load_all_kv_files()
         self.screenmanager = ScreenManager()
         self.screenmanager.add_widget(LoginScreen(name='Login'))
-        self.screenmanager.add_widget(Cadastro(name='cadastro'))
-        self.screenmanager.add_widget(SendCode(name='Send'))
 
         return self.screenmanager
 
     def load_all_kv_files(self):
         Builder.load_file('libs/screens/login_screen/loginscreen.kv')
-        Builder.load_file('libs/screens/Cadastro/cadastro.kv')
-        Builder.load_file('libs/screens/send_code/sendcode.kv')
 
 PrincipalApp().run()
