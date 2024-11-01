9ce472c12d1939a820c83eda10ba7673cd7fa5da
SatoruGojoMaisForte <satorumec@gmail.com>
2024-11-01 18:42:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from kivy.core.window import Window
 
 from libs.screens.login_screen.loginscreen import LoginScreen
 from libs.screens.Cadastro.cadastro import Cadastro
-
+from libs.screens.send_code.sendcode import SendCode
 class PrincipalApp(MDApp):
 
     def build(self):
@@ -14,11 +14,13 @@ class PrincipalApp(MDApp):
         self.screenmanager = ScreenManager()
         self.screenmanager.add_widget(LoginScreen(name='Login'))
         self.screenmanager.add_widget(Cadastro(name='cadastro'))
+        self.screenmanager.add_widget(SendCode(name='Send'))
 
         return self.screenmanager
 
     def load_all_kv_files(self):
         Builder.load_file('libs/screens/login_screen/loginscreen.kv')
         Builder.load_file('libs/screens/Cadastro/cadastro.kv')
+        Builder.load_file('libs/screens/send_code/sendcode.kv')
 
 PrincipalApp().run()
