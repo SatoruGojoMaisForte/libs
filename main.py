3eda35766e319b3e37a7fc752464f5f93d4efcf1
SatoruGojoMaisForte <satorumec@gmail.com>
2024-11-03 09:29:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from libs.screens.login_screen.loginscreen import LoginScreen
 from libs.screens.Cadastro.cadastro import Cadastro
 from libs.screens.check_code.checkcode import CheckCode
 from libs.screens.send_code.sendcode import SendCode
-from libs.screens.write_code.writecode import WriteCode
-from libs.screens.Trocar_senha.trocarsenha import TrocarSenha
+
 class PrincipalApp(MDApp):
 
     def build(self):
@@ -19,8 +18,6 @@ class PrincipalApp(MDApp):
         self.screenmanager.add_widget(Cadastro(name='cadastro'))
         self.screenmanager.add_widget(SendCode(name='Send'))
         self.screenmanager.add_widget(CheckCode(name='Check'))
-        self.screenmanager.add_widget(WriteCode(name='Write'))
-        self.screenmanager.add_widget(TrocarSenha(name='trocar'))
 
         return self.screenmanager
 
@@ -29,7 +26,6 @@ class PrincipalApp(MDApp):
         Builder.load_file('libs/screens/Cadastro/cadastro.kv')
         Builder.load_file('libs/screens/send_code/sendcode.kv')
         Builder.load_file('libs/screens/check_code/checkcode.kv')
-        Builder.load_file('libs/screens/write_code/writecode.kv')
-        Builder.load_file('libs/screens/Trocar_senha/trocarsenha.kv')
+
 
 PrincipalApp().run()
