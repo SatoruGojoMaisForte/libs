d604766e5b31a7dcd2a3f6df9cdd61d73ab7c5ce
SatoruGojoMaisForte <satorumec@gmail.com>
2024-11-05 15:44:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
 from kivy.core.window import Window
+from libs.screens.login_screen.loginscreen import LoginScreen
+from libs.screens.Cadastro.cadastro import Cadastro
 from libs.screens.check_code.checkcode import CheckCode
 from libs.screens.send_code.sendcode import SendCode
 from libs.screens.write_code.writecode import WriteCode
@@ -14,7 +16,8 @@ class PrincipalApp(MDApp):
         Window.size = (400, 800)
         self.load_all_kv_files()
         self.screenmanager = ScreenManager()
-
+        self.screenmanager.add_widget(LoginScreen(name='Login'))
+        self.screenmanager.add_widget(Cadastro(name='cadastro'))
         self.screenmanager.add_widget(SendCode(name='Send'))
         self.screenmanager.add_widget(CheckCode(name='Check'))
         self.screenmanager.add_widget(WriteCode(name='Write'))
@@ -23,6 +26,8 @@ class PrincipalApp(MDApp):
         return self.screenmanager
 
     def load_all_kv_files(self):
+        Builder.load_file('libs/screens/login_screen/loginscreen.kv')
+        Builder.load_file('libs/screens/Cadastro/cadastro.kv')
         Builder.load_file('libs/screens/send_code/sendcode.kv')
         Builder.load_file('libs/screens/check_code/checkcode.kv')
         Builder.load_file('libs/screens/write_code/writecode.kv')
