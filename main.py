f8cef055adb6cd171bdadd5abbf484f8aa336496
SatoruGojoMaisForte <satorumec@gmail.com>
2024-11-28 14:07:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
 from kivy.core.window import Window
-from libs.screens.home_page.homepage import HomePage
+from libs.screens.login_screen.loginscreen import LoginScreen
 
 
 class PrincipalApp(MDApp):
@@ -12,16 +12,13 @@ class PrincipalApp(MDApp):
         self.load_all_kv_files()
 
         self.screenmanager = ScreenManager()
-        self.screenmanager.add_widget(HomePage(name='homepage'))
+        self.screenmanager.add_widget(LoginScreen(name='Login'))
 
         return self.screenmanager
 
     def load_all_kv_files(self):
-        Builder.load_file('libs/screens/home_page/homepage.kv')
-        Builder.load_file('libs/components/circle_perfil.kv')
-        Builder.load_file('libs/components/navigations_buttons,kv')
-        Builder.load_file('libs/components/post_cards.kv')
-        Builder.load_file('libs/components/story_creator.kv')
+        Builder.load_file('libs/screens/login_screen/loginscreen.kv')
+
 
 
 PrincipalApp().run()
