8648760479ae372b8af4445c5ffa4e0546808f0e
SatoruGojoMaisForte <satorumec@gmail.com>
2025-01-11 18:09:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.lang.builder import Builder
 from kivymd.uix.screenmanager import MDScreenManager
-from libs.screens.choice_account.choice_account import ChoiceAccount
-from libs.screens.init_screen.init_screen import InitScreen
-from libs.screens.register_funcionario.register_funcionario import RegisterFuncionario
-from libs.screens.register_contractor.register_contractor import RegisterContractor
-
+from libs.screens.principal_screen.principal_screen import PerfilScreen
 
 
 class MainApp(MDApp):
@@ -14,19 +10,15 @@ class MainApp(MDApp):
     def build(self):
         self.load_all_kv_files()
         self.screenmanager = MDScreenManager()
-        self.screenmanager.add_widget(InitScreen(name='Init'))
-        self.screenmanager.add_widget(ChoiceAccount(name='ChoiceAccount'))
-        self.screenmanager.add_widget(RegisterContractor(name='RegisterContractor'))
-        self.screenmanager.add_widget(RegisterFuncionario(name='RegisterFuncionario'))
-
+        self.screenmanager.add_widget(PerfilScreen(name='Perfil'))
 
         return self.screenmanager
 
     def load_all_kv_files(self):
-        Builder.load_file('libs/screens/init_screen/init_screen.kv')
-        Builder.load_file('libs/screens/choice_account/choice_account.kv')
-        Builder.load_file('libs/screens/register_funcionario/register_funcionario.kv')
-        Builder.load_file('libs/screens/register_contractor/register_contractor.kv')
+        Builder.load_file('libs/screens/principal_screen/principal_screen.kv')
+        Builder.load_file('libs/components/post_cards.kv')
+        Builder.load_file('libs/components/navigations_buttons.kv')
+
 
 
 MainApp().run()
