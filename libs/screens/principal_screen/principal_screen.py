9b53dd3a3463063088f58f12f495d85d5b6ddfaa
SatoruGojoMaisForte <satorumec@gmail.com>
2025-01-12 13:11:35 -0300

    Update principal_screen.py

--- libs/screens/principal_screen/principal_screen.py
+++ libs/screens/principal_screen/principal_screen.py
@@ -1,19 +1,19 @@
-from kivy.properties import Clock
+from kivy.properties import Clock, StringProperty
 from kivy.uix.screenmanager import SlideTransition
 from kivymd.uix.screen import MDScreen
 
 
 class PerfilScreen(MDScreen):
-    username = 'Senhor messias'
-    avatar = 'https://res.cloudinary.com/dsmgwupky/image/upload/v1731366650/image_yljvww.png'
-    regiao = 'GO - Aparecida de Goiania'
-    empresa = 'Porcelanato Liquido'
-    zap = '(62) 99368-3473'
-    email = 'viitiinmec@gmail.com'
-    contratando = 'Sim'
+    username = StringProperty()
+    avatar = StringProperty()
+    regiao = StringProperty()
+    empresa = StringProperty()
+    zap = StringProperty()
+    email = StringProperty()
+    contratando = StringProperty()
 
     def on_enter(self, *args):
-        if self.contratando == 'Não':
+        if self.contratando == 'não':
             self.ids.contratando.text_color = 'red'
         else:
             self.ids.contratando.text_color = 'green'
