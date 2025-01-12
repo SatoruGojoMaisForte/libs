40912fa613743c26581dc17540697a1e127018fc
SatoruGojoMaisForte <satorumec@gmail.com>
2025-01-12 13:13:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from kivy.core.window import Window
 from kivy.lang.builder import Builder
 from kivymd.uix.screenmanager import MDScreenManager
 from libs.screens.principal_screen.principal_screen import PerfilScreen
-
+from libs.screens.edit_profile.edit_profile import EditProfile
 
 class MainApp(MDApp):
 
     def build(self):
+        Window.size = (350, 700)
         self.load_all_kv_files()
         self.screenmanager = MDScreenManager()
+        self.screenmanager.add_widget(EditProfile(name="EditProfile"))
         self.screenmanager.add_widget(PerfilScreen(name='Perfil'))
 
         return self.screenmanager
@@ -18,6 +20,7 @@ class MainApp(MDApp):
         Builder.load_file('libs/screens/principal_screen/principal_screen.kv')
         Builder.load_file('libs/components/post_cards.kv')
         Builder.load_file('libs/components/navigations_buttons.kv')
+        Builder.load_file('libs/screens/edit_profile/edit_profile.kv')
 
 
 
