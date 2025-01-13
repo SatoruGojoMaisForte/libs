17fc10fde9b26912298899b1b8f808c0844bb032
SatoruGojoMaisForte <satorumec@gmail.com>
2025-01-13 18:33:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.lang.builder import Builder
 from kivymd.uix.screenmanager import MDScreenManager
-from libs.screens.principal_screen.principal_screen import PerfilScreen
-from libs.screens.edit_profile.edit_profile import EditProfile
+from libs.screens.permission_screen.permission_screen import PermissionScreen
 
 class MainApp(MDApp):
 
@@ -11,16 +10,12 @@ class MainApp(MDApp):
         Window.size = (350, 700)
         self.load_all_kv_files()
         self.screenmanager = MDScreenManager()
-        self.screenmanager.add_widget(EditProfile(name="EditProfile"))
-        self.screenmanager.add_widget(PerfilScreen(name='Perfil'))
+        self.screenmanager.add_widget(PermissionScreen(name='Permission'))
 
         return self.screenmanager
 
     def load_all_kv_files(self):
-        Builder.load_file('libs/screens/principal_screen/principal_screen.kv')
-        Builder.load_file('libs/components/post_cards.kv')
-        Builder.load_file('libs/components/navigations_buttons.kv')
-        Builder.load_file('libs/screens/edit_profile/edit_profile.kv')
+        Builder.load_file('libs/screens/permission_screen/permission_screen.kv')
 
 
 
