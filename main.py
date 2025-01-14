e2d647fbc375f202e58a16968084c83392213184
SatoruGojoMaisForte <satorumec@gmail.com>
2025-01-14 19:02:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from kivy.lang.builder import Builder
 from kivymd.uix.screenmanager import MDScreenManager
 from libs.screens.permission_screen.permission_screen import PermissionScreen
 from libs.screens.permission_denied.permission_denied import PermissionDenied
+from libs.screens.edit_profile.edit_profile import EditProfile
 
 
 class MainApp(MDApp):
@@ -13,12 +14,14 @@ class MainApp(MDApp):
         self.screenmanager = MDScreenManager()
         self.screenmanager.add_widget(PermissionScreen(name='Permission'))
         self.screenmanager.add_widget(PermissionDenied(name='Denied'))
+        self.screenmanager.add_widget(EditProfile(name='EditProfile'))
 
         return self.screenmanager
 
     def load_all_kv_files(self):
         Builder.load_file('libs/screens/permission_screen/permission_screen.kv')
         Builder.load_file('libs/screens/permission_denied/permission_denied.kv')
+        Builder.load_file('libs/screens/edit_profile/edit_profile.kv')
 
 
 
