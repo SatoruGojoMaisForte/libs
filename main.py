da6211f4f44e92f3b6cdbc204a6c1df6b67bfcdd
SatoruGojoMaisForte <satorumec@gmail.com>
2025-01-15 18:55:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.lang.builder import Builder
 from kivymd.uix.screenmanager import MDScreenManager
-from libs.screens.permission_screen.permission_screen import PermissionScreen
-from libs.screens.permission_denied.permission_denied import PermissionDenied
-from libs.screens.edit_profile.edit_profile import EditProfile
+from libs.screens.confirm_permissions.confirm_permissions import ConfirmPermissions
 
 
 class MainApp(MDApp):
@@ -12,17 +10,11 @@ class MainApp(MDApp):
     def build(self):
         self.load_all_kv_files()
         self.screenmanager = MDScreenManager()
-        self.screenmanager.add_widget(PermissionScreen(name='Permission'))
-        self.screenmanager.add_widget(PermissionDenied(name='Denied'))
-        self.screenmanager.add_widget(EditProfile(name='EditProfile'))
-
+        self.screenmanager.add_widget(ConfirmPermissions(name='Confirm'))
         return self.screenmanager
 
     def load_all_kv_files(self):
-        Builder.load_file('libs/screens/permission_screen/permission_screen.kv')
-        Builder.load_file('libs/screens/permission_denied/permission_denied.kv')
-        Builder.load_file('libs/screens/edit_profile/edit_profile.kv')
-
+        Builder.load_file('libs/screens/confirm_permissions/confirm_permissions.kv')
 
 
 MainApp().run()
