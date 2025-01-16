464890e126acde8e94938e1144282800954f3901
SatoruGojoMaisForte <satorumec@gmail.com>
2025-01-16 18:06:57 -0300

    Update confirm_permissions.py

--- libs/screens/confirm_permissions/confirm_permissions.py
+++ libs/screens/confirm_permissions/confirm_permissions.py
@@ -3,8 +3,9 @@ from plyer import call
 
 
 class ConfirmPermissions(MDScreen):
-    def open_permissions(self):
+    def open_settings(self):
         try:
             call('settings', action='android.intent.action.MAIN')
+
         except NotImplementedError:
             print("Funcionalidade não suportada neste dispositivo.")
