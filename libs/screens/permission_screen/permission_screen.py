c67be46f798d285138971d895bad43c8f763ee90
SatoruGojoMaisForte <satorumec@gmail.com>
2025-01-16 18:37:48 -0300

    Update permission_screen.py

--- libs/screens/permission_screen/permission_screen.py
+++ libs/screens/permission_screen/permission_screen.py
@@ -5,43 +5,10 @@ from android.permissions import request_permissions, check_permission, Permissio
 class PermissionScreen(MDScreen):
 
     def request_permission(self):
-        """
-        Verifica e solicita permissões, se necessário.
-        """
-        if not check_permission(Permission.READ_EXTERNAL_STORAGE) or not check_permission(Permission.WRITE_EXTERNAL_STORAGE):
-            request_permissions(
-                permissions=[
-                    Permission.READ_EXTERNAL_STORAGE,
-                    Permission.WRITE_EXTERNAL_STORAGE
-                ],
-                callback=self.permission_callback
-            )
-            print("Solicitando permissões...")
-        else:
-            print("Permissões já concedidas.")
-            self.on_permissions_granted()
+       try:
+            call('settings', action='android.intent.action.MAIN')
 
-    def permission_callback(self, permissions, results):
-        """
-        Callback chamado após o pedido de permissões.
-        """
-        if all(results):
-            print("Todas as permissões foram concedidas.")
-            self.on_permissions_granted()
-        else:
-            print("Algumas permissões foram negadas.")
-            self.on_permissions_denied()
+        except NotImplementedError:
+            print("Funcionalidade não suportada neste dispositivo.")
 
-    def on_permissions_granted(self):
-        """
-        Ações a serem executadas se as permissões forem concedidas.
-        """
-        print("Permissões concedidas, execute a funcionalidade necessária.")
-        self.manager.current = 'EditProfile'
-
-    def on_permissions_denied(self):
-        """
-        Ações a serem executadas se as permissões forem negadas.
-        """
-        print("Permissões negadas, mostre uma mensagem ou desative a funcionalidade.")
-        self.manager.current = 'Denied'
+    
