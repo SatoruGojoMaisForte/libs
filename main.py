34546c7d0acdb28a7f2a75302b49fff5585a03dc
SatoruGojoMaisForte <satorumec@gmail.com>
2025-02-04 15:50:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ from kivy.core.window import Window
 class MainApp(MDApp):
 
     def build(self):
-        Window.size = (350, 700)
         self.load_all_kv_files()
         self.screenmanager = MDScreenManager()
+        self.screenmanager.add_widget(EditProfileTwo(name='EditProfileTwo'))
         self.screenmanager.add_widget(PerfilScreen(name='Perfil'))
         self.screenmanager.add_widget(EditProfile(name='EditProfile'))
-        self.screenmanager.add_widget(EditProfileTwo(name='EditProfileTwo'))
         return self.screenmanager
 
     def load_all_kv_files(self):
         Builder.load_file('libs/screens/edit_profile/edit_profile.kv')
         Builder.load_file('libs/screens/principal_screen/principal_screen.kv')
         Builder.load_file('libs/screens/edit_profile_two/edit_profile_two.kv')
+        Builder.load_file('libs/components/navigations_buttons.kv')
 
 
 MainApp().run()
