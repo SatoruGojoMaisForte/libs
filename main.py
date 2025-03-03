8251530da71119bf08a2b262a9edceb61fdfa97a
SatoruGojoMaisForte <satorumec@gmail.com>
2025-03-03 15:58:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivymd.uix.screenmanager import MDScreenManager
-from libs.screens.working_days.working_days import WorkingDays
+from libs.screens.working_month.working_month import WorkingMonth
 from kivy.core.window import Window
 
 
@@ -10,11 +10,11 @@ class MainApp(MDApp):
     def build(self):
         self.load_all_kv_files()
         self.screenmanager = MDScreenManager()
-        self.screenmanager.add_widget(WorkingDays(name='WorkingDays'))
+        self.screenmanager.add_widget(WorkingMonth(name='WorkingMonth'))
         return self.screenmanager
 
     def load_all_kv_files(self):
-        Builder.load_file('libs/screens/working_days/working_days.kv')
+        Builder.load_file('libs/screens/working_month/working_month.kv')
 
 
 MainApp().run()
